6cf324f48d1a52aff41409ecb66d690dafbfee28
Roberto Junior <roberto.vrj18@gmail.com>
2024-08-21 20:06:45 -0300

    atualizadao Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        //tipos de variaveis
+
+        short idade = 30;
+        double altura = 1.75;
+        String nome = "Roberto";
+
+        int idade2 = (int) 30; // fazendo casting
+
+        final int ANO_NASCIMENTO = 1993; // representa uma constante, valor que nao pode ser alterado.
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(nome);
+        System.out.println(idade2);
+        System.out.println(ANO_NASCIMENTO);
+
     }
 }
